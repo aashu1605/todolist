e457fe4d9dcc99820aa7d599b01f230c17e5fa50
Darsh Jain <Darsh@Pujas-MacBook-Air.local>
2020-11-11 18:16:39 -0500

    first commit

---
+++ build/precache-manifest.2abbed0d80ba1c0699f3e6a008f6ddac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fcfd60b69e1d155a5091abc5bf13c5b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a29db527bec575b7ac74",
+    "url": "./static/js/2.79900b9e.chunk.js"
+  },
+  {
+    "revision": "646b75339961ed2bca17",
+    "url": "./static/js/main.b59ef0df.chunk.js"
+  },
+  {
+    "revision": "a01e5d1ce2e5b434002d",
+    "url": "./static/js/runtime-main.3b72ba7f.js"
+  }
+]);
